7f9f9d86f26110361559849362bb67638f16600f
Sebastian Gerhards <sebastian.gerhards@aktion-mensch.de>
2015-02-25 15:18:15 +0100

    reformat packages-array

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,19 @@
 #
 class rpmbuild::params {
 
-  $rpmbuild_packages = ['make','automake', 'autoconf', 'gcc', 'gcc-c++', 'rpm-build', 'redhat-rpm-config', 'rpmdevtools', 'yum', 'yum-utils', 'createrepo', 'gnupg2', ]
+  $rpmbuild_packages = [
+    'make',
+    'automake',
+    'autoconf',
+    'gcc',
+    'gcc-c++',
+    'rpm-build',
+    'redhat-rpm-config',
+    'rpmdevtools',
+    'yum',
+    'yum-utils',
+    'createrepo',
+    'gnupg2',
+  ]
   $optional_packages = []
 }
