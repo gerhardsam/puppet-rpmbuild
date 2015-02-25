151eb185386c46c040acefff6951e51f59aaa0d0
Sebastian Gerhards <sebastian.gerhards@aktion-mensch.de>
2015-02-25 15:35:28 +0100

    add test case for custom macro-file

--- spec/defines/env/userhome_spec.rb
+++ spec/defines/env/userhome_spec.rb
@@ -1,44 +1,67 @@
 require 'spec_helper'
 
 describe 'rpmbuild::env::userhome', :type => :define do 
-  
-  let :title do 
-    'testuser'
-  end
-  
-  let :params do
-    {
-      :userfirstname => 'test',
-      :userlastname => 'user',
-      :emailaddress => 'testuser@example.com',
-      :companyname => 'Example Company LLC',
-    }
-  end
-  
-  rpmdirs = [
-    "/home/testuser/rpmbuild",
-    "/home/testuser/rpmbuild/BUILD",
-    "/home/testuser/rpmbuild/RPMS",
-    "/home/testuser/rpmbuild/SOURCES",
-    "/home/testuser/rpmbuild/SPECS",
-    "/home/testuser/rpmbuild/SRPMS", ]
-    
+
+  context 'user-data given' do
+    let :title do
+      'testuser'
+    end
+
+    let :params do
+      {
+          :userfirstname => 'test',
+          :userlastname => 'user',
+          :emailaddress => 'testuser@example.com',
+          :companyname => 'Example Company LLC',
+      }
+    end
+
+    rpmdirs = [
+        "/home/testuser/rpmbuild",
+        "/home/testuser/rpmbuild/BUILD",
+        "/home/testuser/rpmbuild/RPMS",
+        "/home/testuser/rpmbuild/SOURCES",
+        "/home/testuser/rpmbuild/SPECS",
+        "/home/testuser/rpmbuild/SRPMS", ]
+
     rpmdirs.each do |dir|
-      it { should 
+      it { should
       contain_file(dir).with(
-        'ensure' => 'directory',
+          'ensure' => 'directory',
+          'owner' => 'testuser',
+          'group' => 'testuser',
+          'mode' => '0644',
+      )}
+    end
+
+    it { should
+    contain_file('/home/testuser/.rpmmacros').with(
+        'ensure' => 'present',
         'owner' => 'testuser',
         'group' => 'testuser',
         'mode' => '0644',
-      )}
-    end
-    
-    it { should 
-      contain_file('/home/testuser/.rpmmacros').with(
-       'ensure' => 'present',
-       'owner' => 'testuser',
-       'group' => 'testuser',
-       'mode' => '0644',
     )}
-    
-end 
+  end
+
+
+  context 'custom macrofile' do
+    let :params do
+      {
+          :userfirstname => 'test',
+          :userlastname => 'user',
+          :emailaddress => 'testuser@example.com',
+          :companyname => 'Example Company LLC',
+          :usedefaultmacros => 'no',
+          :macrofilepath => '/tmp/test'
+      }
+      it { should contain_file('/home/testuser/.rpmmacros').with(
+          'ensure' => 'present',
+          'owner' => 'testuser',
+          'group' => 'testuser',
+          'mode' => '0644',
+          'source' => '/tmp/test'
+      ) }
+    end
+  end
+
+end
